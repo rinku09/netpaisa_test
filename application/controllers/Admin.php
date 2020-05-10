e79d5e2aa7efba46185fa56f65332149a8ddb481
rinku09 <rinku.raghuwanshi2@gmail.com>
2020-05-11 01:12:59 +0530

    controller comment

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,7 +2,7 @@
 
 error_reporting(0);
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+//for admincontroller
 class Admin extends CI_Controller {
 
 	public function __construct() {
